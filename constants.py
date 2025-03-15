d2530b1508bfc7192d7e78a519bb2e265a07ab19
Aleks Kapich <aleks.kapich@gmail.com>
2025-03-15 18:05:06 +0100

    Add matches for testing

---
+++ constants.py
@@ -0,0 +1,12 @@
+leagues = dict(
+    zip(
+        ["Premier League", "La Liga", "Serie A", "Bundesliga", "Ligue 1"],
+        [
+            "ENG-Premier League",
+            "ESP-La Liga",
+            "ITA-Serie A",
+            "GER-Bundesliga",
+            "FRA-Ligue 1",
+        ],
+    )
+)
