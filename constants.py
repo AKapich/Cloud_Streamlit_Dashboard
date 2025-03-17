bbbae488e612dc9bd4cb8e4f336289623c0d30a7
Aleks Kapich <aleks.kapich@gmail.com>
2025-03-17 18:50:37 +0100

    Add utils

--- constants.py
+++ constants.py
@@ -10,3 +10,29 @@ leagues = dict(
         ],
     )
 )
+
+
+colors = {
+    "Barcelona": "#a4234a",
+    "Valencia": "#fede1b",
+    "Alaves": "#0232a0;",
+    "Las Palmas": "#fee300",
+    "Real Sociedad": "#0067b0",
+    "Juventus": "black",
+    "Como": "#0f4169",
+    "Lecce": "#002755",
+    "Venezia": "#ff6b01",
+    "Genoa": "#00253d",
+    "Augsburg": "#cd1719",
+    "Holstein Kiel": "#00599c",
+    "St. Pauli": "#624739",
+    "Stuttgart": "#d30029",
+    "Hoffenheim": "#165aa4",
+    "Wolfsburg": "#52a500",
+    "Le Havre": "#8bc1e8",
+    "Monaco": "#db0632",
+    "Toulouse": "#433559",
+    "Lens": "#f5d30f",
+    "Marseille": "#009ddc",
+    "Saint-Etienne": "#1d995b",
+}
--- utils.py
+++ utils.py
@@ -5,3 +5,44 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as f:
         encoded = base64.b64encode(f.read()).decode()
     return f"data:image/png;base64,{encoded}"
+
+
+def lighten_hex_color(hex_color, percentage):
+    hex_color = hex_color.lstrip("#")
+    r, g, b = int(hex_color[0:2], 16), int(hex_color[2:4], 16), int(hex_color[4:6], 16)
+    r, g, b = (
+        int(r + (255 - r) * percentage),
+        int(g + (255 - g) * percentage),
+        int(b + (255 - b) * percentage),
+    )
+    r, g, b = min(255, int(r)), min(255, int(g)), min(255, int(b))
+
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
+
+
+def getStartingXI(df, team, deal_with_first_half_subs=True):
+    startingXI = list(
+        set(player for player in df[df["team"] == team]["player"] if player is not None)
+        - set(df[(df["type"] == "SubstitutionOn") & (df["team"] == team)]["player"])
+    )
+    assert len(startingXI) == 11
+
+    if deal_with_first_half_subs:
+        sub_on = list(
+            df[
+                (df["type"] == "SubstitutionOn")
+                & (df["team"] == team)
+                & (df["minute"] < 45)
+            ]["player"]
+        )
+        sub_off = list(
+            df[
+                (df["type"] == "SubstitutionOff")
+                & (df["team"] == team)
+                & (df["minute"] < 45)
+            ]["player"]
+        )
+        startingXI += sub_on
+        startingXI = [player for player in startingXI if player not in sub_off]
+
+    return startingXI
